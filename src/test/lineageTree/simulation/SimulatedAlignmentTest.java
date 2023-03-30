67a6f76053ffc388e5bebf2ba8d3a59162ce05ea
Sophie <seidels@ethz.ch>
2023-03-30 17:35:32 +0200

    Remove dead imports

--- src/test/lineageTree/simulation/SimulatedAlignmentTest.java
+++ src/test/lineageTree/simulation/SimulatedAlignmentTest.java
@@ -3,17 +3,14 @@ package test.lineageTree.simulation;
 import beast.core.parameter.RealParameter;
 import beast.evolution.datatype.DataType;
 import beast.evolution.datatype.IntegerData;
-import beast.evolution.datatype.UserDataType;
 import beast.evolution.sitemodel.SiteModel;
 import beast.evolution.substitutionmodel.Frequencies;
 import beast.evolution.tree.Tree;
 import beast.util.Randomizer;
 import beast.util.TreeParser;
 import feast.fileio.AlignmentFromNexus;
-import feast.simulation.SimulatedAlignment;
 import lineageTree.simulation.SimulatedTypeWriterAlignment;
 import lineageTree.substitutionmodel.TypewriterSubstitutionModel;
-import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
