6be8067f6a1c003009c8bd37f31a700a8a1e69c8
Sophie <seidels@ethz.ch>
2023-03-30 11:08:48 +0200

    Minimal TypewriterData test

--- src/test/TypewriterDataTest.java
+++ src/test/TypewriterDataTest.java
@@ -1,26 +1,6 @@
 package test;
 
 import beast.core.Description;
-import beast.core.parameter.IntegerParameter;
-import beast.core.parameter.RealParameter;
-import beast.evolution.alignment.Alignment;
-import beast.evolution.alignment.Sequence;
-import beast.evolution.branchratemodel.StrictClockModel;
-import beast.evolution.sitemodel.SiteModel;
-import beast.evolution.substitutionmodel.Frequencies;
-import beast.evolution.tree.Tree;
-import beast.util.TreeParser;
-import lineageTree.distributions.TypewriterTreeLikelihood;
-import lineageTree.substitutionmodel.TypewriterSubstitutionModelHomogeneous;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.List;
-
-import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.*;
-import org.junit.Test;
 
 
 @Description("Tests for Typewriter Data on the ancestral state reconstruction and the likelihood")
